eccd9968fa7b1888ad1672308f444f8c4b671c9d
zjun <565061763@qq.com>
2017-12-01 16:23:29 +0800

    重构

--- app/src/main/java/com/fanwe/proxy/Person.java
+++ app/src/main/java/com/fanwe/proxy/Person.java
@@ -1,9 +1,20 @@
 package com.fanwe.proxy;
 
+import android.util.Log;
+
 /**
  * Created by Administrator on 2017/12/1.
  */
 
 public class Person
 {
+
+    public static final String TAG = Person.class.getSimpleName();
+
+    public Object eat(String food)
+    {
+        Log.i(TAG, "eat:" + food);
+        return null;
+    }
+
 }
