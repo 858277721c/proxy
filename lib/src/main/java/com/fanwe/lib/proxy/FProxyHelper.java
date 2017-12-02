8fc4316a5c84b55cc13194a74f516da1209acc7a
zjun <565061763@qq.com>
2017-12-02 10:48:27 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/proxy/FProxyHelper.java
+++ lib/src/main/java/com/fanwe/lib/proxy/FProxyHelper.java
@@ -11,9 +11,9 @@ public class FProxyHelper
      * 通知方法拦截对象
      *
      * @param interceptor
-     * @param proxyClass  代理类
+     * @param proxyClass  代理的class
      * @param methodName  方法名称
-     * @param argsClass   参数类数组
+     * @param argsClass   参数的class组
      * @param argsValue   参数值数组
      * @param proxy       代理对象
      * @return 返回拦截对象的返回值
