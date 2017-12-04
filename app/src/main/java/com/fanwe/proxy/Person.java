9ce69af65211c03f7d71be5a4e5eb42a5612f4b0
zjun <565061763@qq.com>
2017-12-04 09:49:06 +0800

    demo测试

--- app/src/main/java/com/fanwe/proxy/Person.java
+++ app/src/main/java/com/fanwe/proxy/Person.java
@@ -9,23 +9,34 @@ public class Person implements IPerson
 {
     public static final String TAG = Person.class.getSimpleName();
 
+    private String name = TAG;
+
+    public Person()
+    {
+    }
+
+    public Person(String name)
+    {
+        this.name = name;
+    }
+
     @Override
     public void getUp()
     {
-        Log.i(TAG, "getUp");
+        Log.i(TAG, name + " getUp");
     }
 
     @Override
     public float eatFood(String food, int number, float price)
     {
-        Log.i(TAG, "eatFood:" + food + "," + number + "," + price);
+        Log.i(TAG, name + " eatFood:" + food + "," + number + "," + price);
         return number * price;
     }
 
     @Override
     public Book readBook(Book book)
     {
-        Log.i(TAG, "readBook:" + book);
+        Log.i(TAG, name + " readBook:" + book);
         return book;
     }
 }
